b8b19ab9e943f0117e992dd17c3b498f25c895ae
Sridaran Thoniyil <sri7thon@gmail.com>
2023-05-23 03:26:40 -0400

    Change pipeline commands from flags to literals

--- src/bin/monitor-utils.rs
+++ src/bin/monitor-utils.rs
@@ -1,6 +1,8 @@
+use std::ffi::OsString;
+
 use monitor_utils::{x11::XRandrMonitorLoader, Monitor, MonitorSetup, Point};
 
-use bpaf::{construct, long, positional, short, OptionParser, Parser};
+use bpaf::{any, construct, positional, short, OptionParser, Parser};
 
 use anyhow::{anyhow, Result};
 
@@ -36,21 +38,37 @@ fn cli() -> OptionParser<Options> {
         .req_flag(true)
         .fallback(false);
 
-    let clockwise = long("clockwise")
-        .help("Given an argument monitor, yields the next monitor in a clockwise rotation.")
-        .req_flag(Action::NextMonitorClockwise);
+    macro_rules! parse_literal {
+        ($word:expr, $help:expr, $result:expr) => {
+            any($word, |x: OsString| (x == $word).then_some($result.clone())).help($help)
+        };
+    }
 
-    let counter_clockwise = long("counter-clockwise")
-        .help("Given an argument monitor, yields the next monitor in a counter-clockwise rotation.")
-        .req_flag(Action::NextMonitorCounterClockwise);
+    let clockwise = parse_literal!(
+        "clockwise",
+        "Given an argument monitor, yields the next monitor in a clockwise rotation.",
+        Action::NextMonitorClockwise
+    );
 
-    let center = long("center")
-        .help("Given an argument monitor, yields the point at the center of the monitor.")
-        .req_flag(Action::MonitorCenter);
+    let counter_clockwise = parse_literal!(
+        "counter-clockwise",
+        "Given an argument monitor, yields the next monitor in a counter-clockwise rotation.",
+        Action::NextMonitorCounterClockwise
+    );
+
+    let center = parse_literal!(
+        "center",
+        "Given an argument monitor, yields the point at the center of the monitor.",
+        Action::MonitorCenter
+    );
+
+    let monitor_actions = construct!([clockwise, counter_clockwise, center]).group_help("These actions take in monitor arguments through the pipeline. Like before, specify them directly on the command-line without the angle brackets.");
 
     fn monitor_at_point() -> impl Parser<Action> {
-        let monitor_at_point = long("at-point").req_flag(()).group_help(
-            "Takes 2 arguments: X and Y, and yields the monitor containing the point (X,Y)",
+        let monitor_at_point = parse_literal!(
+            "at-point",
+            "Takes 2 arguments: X and Y, and yields the monitor containing the point (X,Y). To use, specify `at-point x y` directly on the command-line without angle-brackets",
+            ()
         );
         let x = positional::<u32>("X");
         let y = positional::<u32>("Y");
@@ -60,7 +78,7 @@ fn cli() -> OptionParser<Options> {
         construct!(Action::MonitorAtPoint(monitor_at_point, point)).adjacent()
     }
 
-    let actions = construct!([clockwise, counter_clockwise, center, monitor_at_point()]).many();
+    let actions = construct!([monitor_at_point(), monitor_actions]).many();
 
     let parser = construct!(Options {
         shell_output,
